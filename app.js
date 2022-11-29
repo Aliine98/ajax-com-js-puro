dcd356937cd5b7bef475894be9527066932acb62
Aline Bevilacqua <Aliine98@users.noreply.github.com>
2022-11-29 14:16:10 -0300

    update

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ function buscaCep() {
    let url = `https://viacep.com.br/ws/${cep}/json`;
    let xhr = new XMLHttpRequest();
    xhr.open('GET', url, true);
+   xhr.send();
    xhr.onreadystatechange = function () {
       if (xhr.readyState == 4 && xhr.status == 200) {
          preencheCampos(JSON.parse(xhr.responseText));
       }
    }
-   xhr.send();
 }
 
 function preencheCampos(json) {

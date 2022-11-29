0e2c957740cc279a5ebd4be38f2f2bf075a932a4
Aline Bevilacqua <Aliine98@users.noreply.github.com>
2022-11-29 13:55:18 -0300

    adicionando função para preencher os campos do formulário com a resposta do viacep

--- app.js
+++ app.js
@@ -9,8 +9,16 @@ function buscaCep() {
    xhr.open('GET', url, true);
    xhr.onreadystatechange = function () {
       if (xhr.readyState == 4 && xhr.status == 200) {
-         console.log(JSON.parse(xhr.responseText));
+         preencheCampos(JSON.parse(xhr.responseText));
       }
    }
    xhr.send();
+}
+
+function preencheCampos(json) {
+   document.querySelector('input[name=endereco]').value = json.logradouro;
+   document.querySelector('input[name=bairro]').value = json.bairro;
+   document.querySelector('input[name=complemento]').value = json.complemento;
+   document.querySelector('input[name=cidade]').value = json.localidade;
+   document.querySelector('input[name=estado]').value = json.uf;
 }
